3a4ab753cad43d79ccc00b29c0b57dfe6c6fe992
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-03-31 17:32:39 -0400

    leaving office, tbc

--- liri.js
+++ liri.js
@@ -1,6 +1,76 @@
 console.log(process.argv);
 
 var command = process.argv[2];
+var query = process.argv[3];
+
+switch(command){
+    case 'my-tweets':
+    	
+        gerTweets();
+
+        break;
+    case 'spotify-this-song':
+        
+        break;
+    case 'movie-this':
+
+        break;
+    case 'do-what-it-says':
+        
+        break;
+}
+// var query = "https://api.twitter.com/1.1/search/tweets.json?q=%40angryjenkins"
+
+	var params = {screen_name: 'nodejs'};
+	client.get('statuses/user_timeline', params, function(error, tweets, response){
+		if (!error) {
+			console.log(tweets);
+	  	}
+});.
+
+}
+
+function getpotifyInfo(){
+
+}
+
+function getMovieInfo(){
+
+}
+
+function getFromRandom(){
+	fs.readFile("bestThingsEver.txt", "utf8", function(err,data){
+		//pull commas and make line breaks (alternate);
+
+		var output = data.split(',');
+
+		for (i=0; i<output.length;i++){
+			console.log(output[i]);
+		}
+
+		var command = output[0];
+		var query = output[1];
+		var fs = require('fs');
+
+		fs.readFile("random.txt", "utf8", function(err,data){
+			var output = data.split(',');
+			var command = output[0].trim();
+			var query = output[1].trim();
+
+
+			for (i=0; i<output.length;i++){
+				console.log(output[i]);
+			}
+
+			// console.log(dataString);
+		});
+	});
+
+}
+
+function logging(command){
+
+}
 
 
 //I need cases for each liri command: my-tweets, spotify-this-song, movie-this, do-what-it-says.
