0c604a5ab5a4ce47fb93158f866d00eb5ed832ea
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-04-04 23:24:17 -0400

    all working, but unable to call other functions in the getFromRandom() function. Also made twitter keys public, ....

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'ykW7lgMUFThqz5IL4q5ScI7Ss',
+  consumer_secret: 'vvZ3YB54OozPZvquxlKVkONVspn6Dzj1wDBnK6XFvSZxyzCuxo',
+  access_token_key: '733105861-FaINs5DuUaEz1leoqgmEHpYFkWryaSpDy3NBVA7F',
+  access_token_secret: '0Jlu8gQbkYp2RYWEEIY8LmRowfcLoR0e0BXC9McqVLOsK'
+}
--- liri.js
+++ liri.js
@@ -1,7 +1,6 @@
 // console.log(process.argv);
 
 var command = process.argv[2];
-var query;
 
 //I need cases for each liri command: my-tweets, spotify-this-song, movie-this, do-what-it-says.
 
@@ -62,8 +61,6 @@ function getTweets(){
   var keys = require('./keys.js');
   var client = new Twitter(keys.twitterKeys);
 
-
-
   // function to pull 20 of my tweets with timestamps.
   var params = {screen_name: query, count: 20};
 
@@ -83,9 +80,6 @@ function getTweets(){
 function getSpotifyInfo(){
   var spotify = require('spotify');
 
-
-
-
   spotify.search({ type: 'track', limit: "1", offset: "1", query: query}, function(err, data) {
       if ( err ) {
           console.log('Error occurred: ' + err);
@@ -95,8 +89,7 @@ function getSpotifyInfo(){
       var spotifyData = data.tracks.items[0];
       var artists = spotifyData.artists;
 
-      console.log(spotifyData);
-      console.log(artists);
+
       console.log('--------');
       console.log('Your Song Query *****');
       console.log('artist: ' + spotifyData.artists[0].name);
@@ -125,8 +118,7 @@ function getMovieInfo(){
 
 
       for (var key in movieData) {
-        if (key ==="Title" || key === "Year" || key === "Rated" || key === "Released" || key === "Director" || key === "Actors" || key === "Plot" || key === "Country" || key === "Language" || key === "imdbRating"
-){
+        if (key ==="Title" || key === "Year" || key === "Rated" || key === "Released" || key === "Director" || key === "Actors" || key === "Plot" || key === "Country" || key === "Language" || key === "imdbRating"){
           if (movieData.hasOwnProperty(key)) {
             //log each object key pair in movieData
             console.log(key + ": " + movieData[key]);
@@ -150,11 +142,70 @@ function getFromRandom(){
     console.log('Query: ' + query);
 
     if(command =='spotify-this-song'){
-      getSpotifyInfo();
+      var spotify = require('spotify');
+
+      spotify.search({ type: 'track', limit: "1", offset: "1", query: query}, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+
+        var spotifyData = data.tracks.items[0];
+        var artists = spotifyData.artists;
+
+
+        console.log('--------');
+        console.log('Your Song Query *****');
+        console.log('artist: ' + spotifyData.artists[0].name);
+        console.log('song title: ' + spotifyData.name);
+        console.log('preview link: ' + spotifyData.external_urls.spotify);
+        console.log('album: ' + spotifyData.album.name);
+        // console.log('release date: ' + data.release_date);
+      });
     } else if (command == 'my-tweets'){
-      getTweets();
+      var Twitter = require('twitter');
+      var keys = require('./keys.js');
+      var client = new Twitter(keys.twitterKeys);
+
+      // function to pull 20 of my tweets with timestamps.
+      var params = {screen_name: query, count: 20};
+
+      client.get('statuses/user_timeline', params, function(error, tweets, response){
+        if (!error) {
+
+          for(i=0; i<tweets.length;i++){
+            console.log("Tweet #" + (i+1));
+            console.log("Screen Name: @" + tweets[i].user.screen_name + " (" + tweets[i].user.name  + ")");
+            console.log("Created: " + tweets[i].created_at);
+            console.log("Tweet: " + tweets[i].text);
+          }
+        }
+      })
     } else if (command == 'movie-this'){
-      getMovieInfo();
+      var request = require('request');
+      var queryURL = 'https://www.omdbapi.com/?type=movie&plot=short&r=json&t=' + query;
+
+      request.get(queryURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var movieData = JSON.parse(response.body);
+
+          // movie name, year, IMDB Rating, Country, Language, Plot, Cast, Rotten Tomatoes Rating, Rotten Tomatoes URL.
+
+          // var movieOutput = ['Title','Year','Rated','Released','Director','Actors','imdbRating','Country','Language'];
+          //would be shorter to say "if key is in movieOutput" below.....
+
+
+          for (var key in movieData) {
+            if (key ==="Title" || key === "Year" || key === "Rated" || key === "Released" || key === "Director" || key === "Actors" || key === "Plot" || key === "Country" || key === "Language" || key === "imdbRating"
+    ){
+              if (movieData.hasOwnProperty(key)) {
+                //log each object key pair in movieData
+                console.log(key + ": " + movieData[key]);
+              }
+            }
+          }
+        }
+      })
     }
   })
 };
