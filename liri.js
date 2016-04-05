a5e3d9f12cf09a273b55ddc5569841e275c8bc7b
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-04-04 21:52:03 -0400

    EVERYTHING WORKS!

--- liri.js
+++ liri.js
@@ -9,6 +9,12 @@ switch(command){
     case 'my-tweets':
     console.log("Getting tweets, ....");
 
+    if(process.argv[3]){
+      var query = process.argv[3];
+    } else {
+      var query = '@angryjenkins';
+    }
+
     getTweets();
     logging();
     break;
@@ -16,12 +22,26 @@ switch(command){
   case 'spotify-this-song':
     console.log("Getting spotify info, ....");
 
+    if(process.argv[3]){
+      var query = process.argv[3];
+    } else if (command =='do-what-it-says'){
+      var query = query;
+    } else {
+      var query = 'whats my age again';
+    }
+
     getSpotifyInfo();
     logging();
     break;
 
   case 'movie-this':
     console.log("Getting OMDB info, ....");
+
+    if(process.argv[3]){
+      var query = process.argv[3];
+    } else {
+      var query = "Mr. Nobody";
+    }
 
     getMovieInfo();
     logging();
@@ -42,11 +62,7 @@ function getTweets(){
   var keys = require('./keys.js');
   var client = new Twitter(keys.twitterKeys);
 
-  if(process.argv[3]){
-    var query = process.argv[3];
-  } else {
-    var query = '@angryjenkins';
-  }
+
 
   // function to pull 20 of my tweets with timestamps.
   var params = {screen_name: query, count: 20};
@@ -67,13 +83,7 @@ function getTweets(){
 function getSpotifyInfo(){
   var spotify = require('spotify');
 
-  if(process.argv[3]){
-    var query = process.argv[3];
-  } else if (command =='do-what-it-says'){
-    var query = query;
-  } else {
-    var query = 'whats my age again';
-  }
+
 
 
   spotify.search({ type: 'track', limit: "1", offset: "1", query: query}, function(err, data) {
@@ -98,11 +108,7 @@ function getSpotifyInfo(){
 };
 
 function getMovieInfo(){
-  if(process.argv[3]){
-    var query = process.argv[3];
-  } else {
-    var query = "Mr. Nobody";
-  }
+
   //  use OMDB API via request npm package to pull:
 
   var request = require('request');
@@ -144,70 +150,11 @@ function getFromRandom(){
     console.log('Query: ' + query);
 
     if(command =='spotify-this-song'){
-      var spotify = require('spotify');
-
-      spotify.search({ type: 'track', limit: "1", offset: "1", query: query}, function(err, data) {
-        if ( err ) {
-            console.log('Error occurred: ' + err);
-            return;
-        }
-
-        var spotifyData = data.tracks.items[0];
-
-        // console.log(spotifyData);
-        console.log('Your Song Query *****');
-        console.log('artist: ' + spotifyData.artists[0].name);
-        console.log('song title: ' + spotifyData.name);
-        console.log('preview link: ' + spotifyData.external_urls.spotify);
-        console.log('album: ' + spotifyData.album.name);
-        // console.log('release date: ' + data.release_date);
-      })
+      getSpotifyInfo();
     } else if (command == 'my-tweets'){
-      var Twitter = require('twitter');
-      var keys = require('./keys.js');
-      var client = new Twitter(keys.twitterKeys);
-
-    
-
-      // function to pull 20 of my tweets with timestamps.
-      var params = {screen_name: query, count: 20};
-
-      client.get('statuses/user_timeline', params, function(error, tweets, response){
-        if (!error) {
-
-          for(i=0; i<tweets.length;i++){
-            console.log("Tweet #" + (i+1));
-            console.log("Screen Name: @" + tweets[i].user.screen_name + " (" + tweets[i].user.name  + ")");
-            console.log("Created: " + tweets[i].created_at);
-            console.log("Tweet: " + tweets[i].text);
-          }
-        }
-      })
+      getTweets();
     } else if (command == 'movie-this'){
-      var request = require('request');
-      var queryURL = 'https://www.omdbapi.com/?type=movie&plot=short&r=json&t=' + query;
-
-      request.get(queryURL, function (error, response, body) {
-        if (!error && response.statusCode == 200) {
-          var movieData = JSON.parse(response.body);
-
-          // movie name, year, IMDB Rating, Country, Language, Plot, Cast, Rotten Tomatoes Rating, Rotten Tomatoes URL.
-
-          // var movieOutput = ['Title','Year','Rated','Released','Director','Actors','imdbRating','Country','Language'];
-          //would be shorter to say "if key is in movieOutput" below.....
-
-
-          for (var key in movieData) {
-            if (key ==="Title" || key === "Year" || key === "Rated" || key === "Released" || key === "Director" || key === "Actors" || key === "Plot" || key === "Country" || key === "Language" || key === "imdbRating"
-    ){
-              if (movieData.hasOwnProperty(key)) {
-                //log each object key pair in movieData
-                console.log(key + ": " + movieData[key]);
-              }
-            }
-          }
-        }
-      })
+      getMovieInfo();
     }
   })
 };
