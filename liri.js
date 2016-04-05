9f6847d8db570406be8cc1dea5d3f627d30d5640
M. Nathaniel Martin <matt.martin@rutgers.edu>
2016-04-04 21:46:52 -0400

    Homework is working! May want to re-arrange where queries are defined. Defining queries in functions causes problems for the 'do-what-it-says' command.

--- liri.js
+++ liri.js
@@ -143,24 +143,72 @@ function getFromRandom(){
     console.log('Command: ' + command);
     console.log('Query: ' + query);
 
-    var spotify = require('spotify');
+    if(command =='spotify-this-song'){
+      var spotify = require('spotify');
 
-    spotify.search({ type: 'track', limit: "1", offset: "1", query: query}, function(err, data) {
-      if ( err ) {
-          console.log('Error occurred: ' + err);
-          return;
-      }
+      spotify.search({ type: 'track', limit: "1", offset: "1", query: query}, function(err, data) {
+        if ( err ) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
 
-      var spotifyData = data.tracks.items[0];
+        var spotifyData = data.tracks.items[0];
 
-      // console.log(spotifyData);
-      console.log('Your Song Query *****');
-      console.log('artist: ' + spotifyData.artists[0].name);
-      console.log('song title: ' + spotifyData.name);
-      console.log('preview link: ' + spotifyData.external_urls.spotify);
-      console.log('album: ' + spotifyData.album.name);
-      // console.log('release date: ' + data.release_date);
-    })
+        // console.log(spotifyData);
+        console.log('Your Song Query *****');
+        console.log('artist: ' + spotifyData.artists[0].name);
+        console.log('song title: ' + spotifyData.name);
+        console.log('preview link: ' + spotifyData.external_urls.spotify);
+        console.log('album: ' + spotifyData.album.name);
+        // console.log('release date: ' + data.release_date);
+      })
+    } else if (command == 'my-tweets'){
+      var Twitter = require('twitter');
+      var keys = require('./keys.js');
+      var client = new Twitter(keys.twitterKeys);
+
+    
+
+      // function to pull 20 of my tweets with timestamps.
+      var params = {screen_name: query, count: 20};
+
+      client.get('statuses/user_timeline', params, function(error, tweets, response){
+        if (!error) {
+
+          for(i=0; i<tweets.length;i++){
+            console.log("Tweet #" + (i+1));
+            console.log("Screen Name: @" + tweets[i].user.screen_name + " (" + tweets[i].user.name  + ")");
+            console.log("Created: " + tweets[i].created_at);
+            console.log("Tweet: " + tweets[i].text);
+          }
+        }
+      })
+    } else if (command == 'movie-this'){
+      var request = require('request');
+      var queryURL = 'https://www.omdbapi.com/?type=movie&plot=short&r=json&t=' + query;
+
+      request.get(queryURL, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+          var movieData = JSON.parse(response.body);
+
+          // movie name, year, IMDB Rating, Country, Language, Plot, Cast, Rotten Tomatoes Rating, Rotten Tomatoes URL.
+
+          // var movieOutput = ['Title','Year','Rated','Released','Director','Actors','imdbRating','Country','Language'];
+          //would be shorter to say "if key is in movieOutput" below.....
+
+
+          for (var key in movieData) {
+            if (key ==="Title" || key === "Year" || key === "Rated" || key === "Released" || key === "Director" || key === "Actors" || key === "Plot" || key === "Country" || key === "Language" || key === "imdbRating"
+    ){
+              if (movieData.hasOwnProperty(key)) {
+                //log each object key pair in movieData
+                console.log(key + ": " + movieData[key]);
+              }
+            }
+          }
+        }
+      })
+    }
   })
 };
 
